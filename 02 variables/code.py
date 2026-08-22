720f54ce4dc27d5ec9cdc1916928f8dda8845649
ashok khoja <ashok.khoja.cg@gmail.com>
2026-08-22 16:41:01 +0530

    new files added

--- 02 variables/code.py
+++ 02 variables/code.py
@@ -3,28 +3,35 @@ name="Ashok"
 age="17"
 city="nagaur"
 print(name,age,city)
+
 #Q=25
 student_name="Ashok khoja"
 student_roll_number="0040"
 student_branch="B.TECH"
 print(student_name,student_roll_number,student_branch)
+
 #Q=26
 marks=50
 marks=500
 print(marks)
+
 #Q=27
 name,age,city="rahul","18","patna"
 print(name,age,city)
+
 #Q=28
 x=y=z=0
 print(x,y,z)
+
 #Q=29
 student1="Rahul"
 student_name="rahul"
 Class ="btech"
+
 #Q=30
 student_name="ashok khoja"
 student_age="17"
 student_age="18"
 print(student_name,student_age)
+
 
