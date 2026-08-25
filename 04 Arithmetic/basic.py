f7128cd1d932b212b149d0172fff2241c3fd768b
Rahul Sharma <rahul@example.com>
2026-08-25 23:16:35 +0530

    new folder added

---
+++ 04 Arithmetic/basic.py
@@ -0,0 +1,185 @@
+# Task=1
+a=20
+b=15
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+# Task=2
+a=10
+b=20.0
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+# Task=3
+maths=20
+physics=5
+english=10
+total=maths+physics+english
+print(total)
+averge=total/3
+print(averge)
+print(35/3)
+# Task=4
+priceofonion=10
+priceofchili=15
+priceoftomato=20
+print(priceofonion+priceofchili+priceoftomato)
+#total prise
+ # Task=5
+a=10
+b=2
+print(a%b)
+a=10
+b=3
+print(a%b)
+# Task=6
+a=64
+b=16
+print(a/b)
+print(a//b)
+a=-64
+b=16
+print(a/b)
+print(a//b)
+# Task=7
+a=-15
+b=-25
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+# Task=08
+a=50
+b=25
+print(a-b)
+a=60
+b=-20
+print(a-b)
+a=-30
+b=50
+print(a-b)
+a=-50
+b=-25
+print(a-b)
+# Task =08
+a=25
+b=5
+print(a//b)
+a=-25
+b=5
+print(a//b)
+a=25
+b=-5
+print(a//b)
+a=-25
+b=-5
+print(a//b)
+# Task=10
+a=20
+b=5
+print(a%b)
+a=-20
+b=5
+print(a%b)
+a=30
+b=-15
+print(a%b)
+a=-45
+b=-15
+print(a%b)
+# Task=11
+print(10+5*2)
+print(20-4/2)
+print(10+20/5*2)
+print(2+3*4**2)
+print(100-20//5)
+# Task=12
+print(10 + 5 * 2)
+print((10+5)*2)
+print(20-10/2)
+print((20-10)/2)
+print(2+3*4)
+print((2+3)*4)
+# Task=13
+
+
+print(True+False)
+print(True-False)
+print(True*False)
+print(False/True)
+print(False//True)
+print(False%True)
+print(True**False)
+# Task=14
+print(True+5)
+print(False+5)
+print(True*10)
+print(False*10)
+print(True-5)
+print(False-5)
+#Task=15
+First_Name="ashok"
+Last_Name="khoja"
+print(First_Name +"  "+ Last_Name)
+#Task=16
+a="ak"
+print(a*3)
+# Task=17
+a="15"
+b="15"
+print(a+b)
+print("Outpute for task-17:",name+" "+lname,(name+" "+lname)*4)
+print("The subtraction and division show the Type error.....")
+
+
+
+
+#Task 18
+value = None
+num=12
+print("Outpute for task-18:"," Python show -TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'")
+#Task=19
+x=10000000
+y=0
+print("Outpute for task-19:","Python shows - ZeroDivisionError: division by zero")
+
+#Task 20 — Mini Calculator
+num1 = 10
+num2 = 5
+
+print("Outpute for task-20:","Addition: ...",num1 + num2)
+print("Outpute for task-20:","Subtraction: ...",num1 - num2)
+print("Outpute for task-20:","Multiplication: ...",num1 * num2)
+print("Outpute for task-20:","Division: ...",num1 / num2)
+print("Outpute for task-20:","Floor Division: ...",num1//num2)
+print("Outpute for task-20:","Modulus: ...",num1%num2)
+print("Outpute for task-20:","Exponentiation: ...",num1**num2)
+
+#Part 10 — Final Challenge
+#Task 21 — Arithmetic Expression Analyzer
+a = 10
+b = -3
+c = 2.5
+print("Outpute for task-21:")
+print(a + b)
+print(a - b)
+print(a * c)
+print(a / c)
+print(a // b)
+print(a % b)
+print(a ** 2)
+print((a + b) * c)
+print(a + b * c)
+print((a - b) / c)
+print(a ** 2 + b * c)
+print((a + c) // 2)
