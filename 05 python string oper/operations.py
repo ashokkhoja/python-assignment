f85d0e09ef3ed1e33e88c43ce5b3a5415212f78b
Rahul Sharma <rahul@example.com>
2026-09-01 20:25:45 +0530

    added

--- 05 python string oper/operations.py
+++ 05 python string oper/operations.py
@@ -136,6 +136,119 @@ print(A.count("a"))
 print(A.count("n"))
 print(A.count("b"))
 
+#Task 24 — Starts and Ends
+filename = "student_notes.pdf"
+a=filename.startswith("student")
+b=filename.endswith(".pdf")
+c=filename.endswith(".txt")
+print(a)
+print(b)
+print(c)
+
+#Task 25 — Replace a Word
+text = "I am learning Java"
+a=text.replace("Java","python")
+print(a)
+
+#Task 26 — Multiple Replacements
+text = "apple apple apple"
+a=text.replace("apple","mango",3)
+print(a)
+
+#Task 27 — Limited Replacement
+text = "apple apple apple"
+a=text.replace("apple","mango",1)
+print(a)
+
+#Task 28 — Check Immutability
+text = "Python"
+b=text.upper()
+print(b)
+print(text)
+
+#Part 12 — Whitespace
+#Task 29
+text = "   Python Programming   "
+a=text.strip()
+print(a)
+b=text.lstrip()
+print(b)
+c=text.rstrip()
+print(c)
+
+#Task 30 — User Input
+
+
+#Part 13 — Split and Join
+#Task 31 — Split
+a="Python is easy to learn"
+print(a.split())
+
+#Task 32 — Split with Separator
+b="apple,banana,mango,orange"
+print(b.split(","))
+
+#Task 33 — Join
+words = ["Python", "is", "easy"]
+print(" ".join(words))
+
+#Task 34 — Join with Different Separators
+words = ["Python", "is", "easy"]
+print("-".join(words))
+print("/".join(words))
+
+#Part 14 — String Formatting
+#Task 35 — F-String
+name="ashok"
+age="17"
+city="nagaur"
+print(f"my name is {name} i old in {age} i live in {city}.")
+
+#Task 36 — Arithmetic Inside F-String
+a = 10
+b = 20
+c=10+20
+print(f"The sum is {c}")
+
+#Part 15 — Error Identification
+#Task 37
+text = "Python"
+print(text[0:])
+#indexerror in print in task 37
+
+text = "Python"
+
+#print(sytanx error in task37)
+print("j"+text[1:])
+
+age = 20
+print("Age: " +str ("age"))
+#print(TypeError in)
+
+text = "Python"
+print(text.index("Python"))
+#print(valueerror)
+
+#Part 16 — Practical Challenge
+#Task 38 — Name Processor
+
+name= input("Enter your full name:")
+user=name.strip()
+print(name)
+print(user)
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(len(name))
+print(name[0])
+print(name[-1])
+print("a" in name)
+
+#art 17 — Practical Challenge
+#Task 39 — Sentence Analyzer
+
+a=input("enter sentence")
+print(a.strip())
 
 
 
