41fb80366215194a0840507eba75603ea1beebdf
Rahul Sharma <rahul@example.com>
2026-09-04 18:37:33 +0530

    added

---
+++ 06 boolean/boolean.py
@@ -0,0 +1,131 @@
+#Q=1
+a="true"
+b="false"
+print(a,b)
+
+#Q=2,3
+boolean=True,False
+print(boolean)
+
+#Q=4
+a=True
+b="True"
+print(a="in boolean ",b="string")
+#Q=5
+a=true
+print(beacuse in python in case sensitivy)
+#Q=6
+print(10==10 ) 
+print(type(true))
+#Q=7
+print(10!=5)
+print(True)
+#Q=8
+print(5>10)
+print(True)
+#Q=9
+print(5<10)
+print(True)
+#Q=10
+print(18>=18)
+print(True)
+#Q=11
+print(18<=10)
+print(True)
+#Q=12
+print("=" in assignment)
+print("==" in comprision)
+#Q=13
+print(ago in before check in example)
+#Q=14
+print("10+5 addition,10>5 boolean,python in string ,10==20 boolean")
+#Q=15
+age = 20
+print(age >= 18)
+print(True)
+#Q=16
+marks = 45
+print(marks == 50)
+print(False)
+#Q=17
+age=19
+print(19>=18)
+print(True)
+#Q=18
+a=True and True
+b=True and False
+c=False and True
+d=False and False
+print(a=T,b=F,c=F,d=F)
+#Q=19
+a=True or True
+b=True or False
+c=False or True
+d=False or False
+print(a=T,b=T,c=T,d=F)
+#Q=20
+a=not True
+b=not False
+print(a=F,b=T)
+#Q=21
+print(reading)
+#Q=22
+age = 25
+
+print(age >= 18 and age <= 60)
+print(True)
+#Q=23
+age = 16
+
+print(age < 18 or age > 60)
+print(True)
+#Q=24
+age = 20
+
+print(not age < 18)
+print(False)
+#Q=25
+a=100
+print(a>10 and a<50)
+print(False)
+# Q=26
+b=150
+print(b<10 and b>100 )
+print(False)
+#Q=27
+print(not(0 and 1 or none and ""))
+print(f or f)
+print(not(f))
+print(t)
+#Q=28
+print(bool(1))
+print(bool(-5))
+print(bool(""))
+print(bool("Python"))
+print(bool(False))
+print(bool(True))
+print(bool(None))
+
+#Q=29
+print(bool(0))
+print(bool(10))
+print(bool(""))
+print(bool("Hello"))
+print(bool(None))
+
+#Q=30
+
+print(bool(0),type(0))
+print(bool(1),type(1))
+print(bool(""),type(""))
+print(bool("Python"),type("Python"))
+print(bool(False),type(False))
+print(bool(None),type(None))
+
+
+
+
+
+
+
+
