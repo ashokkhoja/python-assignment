d21a9bdf21d1e61261596fe0c4435d9a8fd49a2b
Rahul Sharma <rahul@example.com>
2026-09-23 18:23:31 +0530

    new added

---
+++ 00 problem/02 nestesd_for_loop.py
@@ -0,0 +1,171 @@
+# 1. Print a 3×3 Star Grid
+# for i in range(1,4):
+#     for j in range(3):
+#         print("*",end="")
+#     print()
+
+# 2. Print Numbers in Rows
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(j,end="")
+#     print()
+
+# 3. Print Row Numbers
+# for i in range(1,4):
+#    for j in range(1,4):
+#     print(i,end="")
+#    print()
+
+# 4. Increasing Star Pattern
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print("* ",end="")
+#     print()
+
+    # 5. Decreasing Star Pattern
+
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("* ",end="")
+#     print()
+
+#  6. Increasing Number Pattern
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+#  7. Repeated Number Pattern
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#        print(i,end="")
+#     print()
+
+#  8. Multiplication Tables from 1 to 5
+
+# for i in range(1,6):
+#     for j in range(1,11):
+#         print(f"{i}*{j}={i*j}")
+#     print()
+
+# 9. Multiplication Grid
+
+# for i in range(1,4):
+#     for j in range(1,6):
+#         print(i*j,end=" ")
+#     print()
+
+
+# 10. Print Squares in Rows
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j**2,end=" ")
+#     print()
+
+
+# 11. Alphabet Pattern
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print(chr(65+j),end=" ")
+#     print()
+
+# 12. Repeated Alphabet Pattern
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print(chr(65+i),end=" ")
+#     print()
+
+# #  13. Odd Number Pattern
+# for i in range(5):
+#     for j in range(1,(i+1)*2,2):
+#         print(j,end=" ")
+#     print()
+
+# 14. Even Number Pattern
+
+# for i in range(5):
+#     for j in range(1,(i+1)*2,2):
+#         print(j+1,end=" ")
+#     print()
+
+# 15. 5×5 Star Square
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print("* ",end="")
+#     print()
+
+# 16. 5×5 Number Square
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j,end=" ")
+#     print()
+
+
+## 17. Row-wise Numbers
+# p=1
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(p,end=" ")
+#         p+=1
+#     print()
+
+
+# 18. Print 1 to 20 in 4 Rows
+# p=1
+# for i in range(4):
+#     for j in range(5):
+#         print(p,end=" ")
+#         p+=1
+#     print()
+
+#  19. Print Coordinate Pairs
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(f"({i},{j})",end=" ")
+#     print()
+
+#  20. Print All Number Combinations
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i,j)
+  
+#  21. 10×10 Multiplication Grid
+
+# for i in range(1,11):
+#     for j in range(1,11):
+#          print(f"{i}*{j}={i*j}")
+#     print()
+
+#  22. Repeated Number Pattern
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+# 23. Decreasing Number Pattern
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+#  24. Reverse Number Pattern
+# for i in range(5):
+#     for j in range(5-i):
+#         print(5-j,end="")
+#     print()
+
+
+# 25. Repeated Row Number Pattern
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i,end="")
+#     print()
---
+++ 00 problem/03.for_loops.py
@@ -0,0 +1,346 @@
+# # 1. Digit and chrracter Analyzer
+# uppercasecount=0
+# lowercount=0
+# spacescount=0
+# specialcount=0
+# digitscount=0
+# string=input("enter in chrracters:")
+# for i in string:
+#     if chr(65)<=i<=chr(90):
+#         print("uppercase:")
+#         uppercasecount+=1
+#     elif chr(97)<=i<=chr(122):
+#         print("lowercase:")
+#         lowercount+=1
+#     elif chr(48)<=i<=chr(57): 
+#         print("digits:")
+#         digitscount+=1
+#     elif chr(32)==i:
+#         print("space")
+#         spacescount+=1
+#     else:
+#         print("special")
+#         specialcount+=1
+# if uppercasecount>lowercount and uppercasecount>digitscount and uppercasecount>spacescount and uppercasecount>specialcount:
+#     print("uppercase highest")
+# elif lowercount>uppercasecount and lowercount>digitscount and lowercount>specialcount and lowercount>spacescount:
+#     print("loewrcase highest")
+# elif spacescount>uppercasecount and spacescount>digitscount and spacescount>specialcount and spacescount>lowercount:
+#     print("spacescount highest")
+# elif specialcount>uppercasecount and specialcount>lowercount and specialcount>digitscount and specialcount>spacescount:
+#     print("specialcount highest")
+# elif digitscount>uppercasecount and digitscount>lowercount and digitscount>spacescount and digitscount>specialcount:
+#     print("digitscount highest")
+# else:
+#     print("Tie")
+
+
+# print(uppercasecount)
+# print(lowercount)
+# print(digitscount)
+# print(spacescount)
+# print(specialcount)
+
+# 2. Student Performance Analyzer
+# excellentcount=0
+# goodcount=0
+# passcount=0
+# failcount=0
+# for i in range(1,11):
+#     marks=int(input("enter in marks:"))
+#     if 75<=marks<=100:
+#         print(("excellent"))
+#         excellentcount+=1
+#     elif 50<=marks<=74:
+#         print("Good")
+#         goodcount+=1
+#     elif 35<=marks<=49:
+#         print("Pass")
+#         passcount+=1
+#     else:
+#         print("fail")
+#         failcount+=1
+
+#     print(excellentcount)
+#     print(goodcount)
+#     print(passcount)
+#     print(failcount)
+
+
+# 3. chr Score Calculator
+sentence=input("enter in sentence:")
+if chr=="AEIOU":
+  Print("vowel")
+elif chr!="AEIOU":
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 25. Number Pyramid With Classification
+# n=int(input("enter in numbers:-"))
+# for i in range(1,n):
+#     for j in range(1,n+1):
+#      if j%2==0:
+#         if j%3:
+#          print("T",end="")
+#          print(j,"E",end="")
+#         elif j%3 or j%5:
+#            print("F",end="")
+#      elif j%2!=0:
+#         print(j,"O",end="")
+#     print()
+   
+
